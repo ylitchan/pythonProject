ee7dc40f0c25369746abac6a0a041740eb432d3a
ylitchan <898475174@qq.com>
2024-03-29 17:08:35 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/BN.py
+++ tokenDemo/BN.py
@@ -85,7 +85,8 @@ def job():
             price_open = kline_hour[-1][1]
             vol = kline_hour[kline_vol][5]
             # 反包之前K线,缩半量,爆倍量
-            if price_close >= max(kline_hour[:-1], key=lambda x: x[2])[2] and vol >= kline_hour[-1][5] * 2 and vol >= \
+            if price_close >= max(kline_hour[:-1], key=lambda x: x[4])[4] and vol >= kline_hour[-1][
+                5] * 2 and vol >= \
                     max(kline_hour[:kline_vol], key=lambda x: x[5])[5] * 2:
                 # 爆量之后阳K数量
                 boom = sum(1 for sublist in kline_hour[kline_vol:-1] if sublist[4] >= sublist[1])
