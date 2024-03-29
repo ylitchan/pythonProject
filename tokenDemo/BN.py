40cb2982f44044f146f1729d18a630ed052c3a07
ylitchan <898475174@qq.com>
2024-03-29 11:04:57 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/BN.py
+++ tokenDemo/BN.py
@@ -153,6 +153,6 @@ def job():
 if __name__ == "__main__":
     job()
     # 设置任务调度
-    scheduler.add_job(job, 'cron', minute='00', second='10')
+    scheduler.add_job(job, 'cron', minute='00', second='3')
     # 启动调度器
     scheduler.start()
