7379da7501c11df5f70129d46bdb7d066ba73458
ylitchan <898475174@qq.com>
2024-04-07 14:25:32 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/BN.py
+++ tokenDemo/BN.py
@@ -81,10 +81,9 @@ def job():
                 # 第一个倍量所在索引
                 # kline_vol = max(range(len(kline_hour)), key=lambda x: kline_hour[x][5])
                 kline_vol = next(filter(
-                    lambda x: price_close >= kline_hour[x][4] and kline_hour[x][4] / kline_hour[x - 1][
-                        4] - 1 >= 0.0299 and kline_hour[x][5] >= max(
-                        max(kline_hour[:x], key=lambda y: y[5])[5], kline_hour[-1][5]) * 2 and
-                              kline_hour[x][4] >= max(kline_hour[:x], key=lambda y: y[2])[2], range(3, 7)), None)
+                    lambda x: kline_hour[x][4] / kline_hour[x - 1][4] - 1 >= 0.0299 and price_close >= kline_hour[x][
+                        4] >= max(kline_hour[:x], key=lambda y: y[2])[2] and kline_hour[x][5] >= max(
+                        max(kline_hour[:x], key=lambda y: y[5])[5], kline_hour[-1][5]) * 2, range(3, 7)), None)
                 if kline_vol:
                     cc = symbol[:-4] in symbols_asset
                     alert_b.append(
