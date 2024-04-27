0da7a14ba4049ab66de23226ec0b92d2023fd0c2
ylitchan <898475174@qq.com>
2024-04-27 17:06:58 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/BN.py
+++ tokenDemo/BN.py
@@ -95,23 +95,25 @@ def rzq():
 
 
 def bn(symbol, symbols_asset, alert_b):
-    try:
-        kline_hour = [[float(i) for i in sub] for sub in client.klines(symbol=symbol, interval="15m", limit=4)[:3]]
-        price_close = kline_hour[-1][4]
-        price_open = kline_hour[-1][1]
-        if price_close >= price_open:
-            # 第一个倍量所在索引
-            kline_vol = next(filter(
-                lambda x: kline_hour[x][4] / kline_hour[x - 1][4] >= 1.0333687020354 and price_close >= kline_hour[x][
-                    4] >= max(kline_hour[:x], key=lambda y: y[2])[2] and kline_hour[x][5] >= max(
-                    max(kline_hour[:x], key=lambda y: y[5])[5], kline_hour[-1][5]), range(1, 2)), None)
-            if kline_vol:
-                cc = symbol[:-4] in symbols_asset
-                alert_b.append(
-                    (symbol, price_close, (kline_hour[kline_vol][4] / kline_hour[kline_vol - 1][4] - 1) * 100, cc,
-                     price_close * 1.0333687020354))
-    except Exception as e:
-        print(str(e))
+    for i in range(10):
+        try:
+            kline_hour = [[float(i) for i in sub] for sub in client.klines(symbol=symbol, interval="15m", limit=4)[:3]]
+            price_close = kline_hour[-1][4]
+            price_open = kline_hour[-1][1]
+            if price_close >= price_open:
+                # 第一个倍量所在索引
+                kline_vol = next(filter(
+                    lambda x: kline_hour[x][4] / kline_hour[x - 1][4] >= 1.0333687020354 and price_close >= kline_hour[x][
+                        4] >= max(kline_hour[:x], key=lambda y: y[2])[2] and kline_hour[x][5] >= max(
+                        max(kline_hour[:x], key=lambda y: y[5])[5] * 2, kline_hour[-1][5]), range(1, 2)), None)
+                if kline_vol:
+                    cc = symbol[:-4] in symbols_asset
+                    alert_b.append(
+                        (symbol, price_close, (kline_hour[kline_vol][4] / kline_hour[kline_vol - 1][4] - 1) * 100, cc,
+                         price_close * 1.0333687020354))
+            break
+        except Exception as e:
+            print(str(e))
 
 
 def job():
