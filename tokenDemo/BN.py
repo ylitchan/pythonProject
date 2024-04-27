1a5c4a5a5f9364ab284b1c27c6f9a48b358444e1
ylitchan <898475174@qq.com>
2024-04-27 23:27:55 +0800

    y

--- tokenDemo/BN.py
+++ tokenDemo/BN.py
@@ -97,16 +97,15 @@ def rzq():
 def bn(symbol, symbols_asset, alert_b):
     for i in range(10):
         try:
-            kline_hour = [[float(i) for i in sub] for sub in client.klines(symbol=symbol, interval="15m", limit=4)[:3]]
+            kline_hour = [[float(i) for i in sub] for sub in client.klines(symbol=symbol, interval="15m", limit=6)[:5]]
             price_close = kline_hour[-1][4]
             price_open = kline_hour[-1][1]
             if price_close >= price_open:
                 # 第一个倍量所在索引
                 kline_vol = next(filter(
-                    lambda x: kline_hour[x][4] / kline_hour[x - 1][4] >= 1.0333687020354 and price_close >=
-                              kline_hour[x][
-                                  4] >= max(kline_hour[:x], key=lambda y: y[2])[2] and kline_hour[x][5] >= max(
-                        max(kline_hour[:x], key=lambda y: y[5])[5] * 2, kline_hour[-1][5]), range(1, 2)), None)
+                    lambda x: kline_hour[x][4] / kline_hour[x - 1][4] >= 1 and price_close >=
+                              kline_hour[x][4] >= max(kline_hour[:x], key=lambda y: y[2])[2] and kline_hour[x][5] >= max(
+                        max(kline_hour[:x], key=lambda y: y[5])[5] * 10, kline_hour[-1][5]), range(4, 5)), None)
                 if kline_vol:
                     # try:
                     #     res = client.new_order(symbol=symbol, side='BUY', type='MARKET', timeInForce="GTC",
@@ -122,7 +121,7 @@ def bn(symbol, symbols_asset, alert_b):
                     #     print(str(e))
                     alert_b.append(
                         (symbol, price_close, (kline_hour[kline_vol][4] / kline_hour[kline_vol - 1][4] - 1) * 100,
-                         symbol[:-4] in symbols_asset, price_close * 1.0333687020354))
+                         symbol[:-4] in symbols_asset, price_close * 1.10333687020354))
             break
         except Exception as e:
             print(str(e))
