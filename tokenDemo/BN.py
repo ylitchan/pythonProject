81d9780cfc9c041e861751a08ce6ddf1874a6757
ylitchan <898475174@qq.com>
2024-04-29 20:53:07 +0800

    y

--- tokenDemo/BN.py
+++ tokenDemo/BN.py
@@ -79,7 +79,12 @@ def rzq():
                     {
                         "title": "会所嫩模领取",
                         "description": "",
-                        "url": "https://www.iwencai.com/unifiedwap/result?w=%E6%98%A8%E6%97%A5%E7%88%86%E9%87%8F%E6%B6%A8%E5%81%9C%3B%E4%BB%8A%E6%97%A5%E7%AB%9E%E4%BB%B7%E9%AB%98%E5%BC%80%3B%E7%AB%9E%E4%BB%B7%E5%BC%82%E5%8A%A8%E8%AF%B4%E6%98%8E%3B%E6%98%A8%E6%97%A5%E6%B6%A8%E5%81%9C%E5%8E%9F%E5%9B%A0%E7%B1%BB%E5%88%AB%3B%E9%9B%86%E5%90%88%E7%AB%9E%E4%BB%B7%E8%AF%84%E7%BA%A7%E5%8C%85%E5%90%AB%E7%9C%8B%E5%A4%9A%E6%88%96%E8%80%85%E5%81%8F%E5%A4%9A&querytype=stock",
+                        "url": "https://www.iwencai.com/unifiedwap/result?w=%E6%98%A8%E6%97%A5%E7%88%86%E9%87%8F%E6"
+                               "%B6%A8%E5%81%9C%3B%E4%BB%8A%E6%97%A5%E7%AB%9E%E4%BB%B7%E9%AB%98%E5%BC%80%3B%E7%AB%9E"
+                               "%E4%BB%B7%E5%BC%82%E5%8A%A8%E8%AF%B4%E6%98%8E%3B%E6%98%A8%E6%97%A5%E6%B6%A8%E5%81%9C"
+                               "%E5%8E%9F%E5%9B%A0%E7%B1%BB%E5%88%AB%3B%E9%9B%86%E5%90%88%E7%AB%9E%E4%BB%B7%E8%AF%84"
+                               "%E7%BA%A7%E5%8C%85%E5%90%AB%E7%9C%8B%E5%A4%9A%E6%88%96%E8%80%85%E5%81%8F%E5%A4%9A"
+                               "&querytype=stock",
                         "picurl": "https://img11.chkaja.com/files/20240402/cf65830bbf07486c.jpg"
                     }
                 ]
