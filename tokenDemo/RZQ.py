d4d207deca8e7a7173b46ab94899db43931911c9
ylitchan <898475174@qq.com>
2024-06-17 10:09:38 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/RZQ.py
+++ tokenDemo/RZQ.py
@@ -70,13 +70,14 @@ def rzq_token(symbol, alert, success):
                 kline_hour = [list(map(float, sublist)) for sublist in
                               client.klines(symbol=symbol, interval="4h", limit=22)]
             price_close = kline_hour[-1][4]
+            price_zy = price_close * 1.04
             price_vol = kline_hour[-2][4]
             zf = (price_vol / kline_hour[-3][4] - 1) * 100
-            # 止盈涨幅6个点，止损亏4个点
-            if (zf >= 4 and price_vol * 1.04 > kline_hour[-1][2] and price_close >= price_vol >=
+            # 止盈止损亏4个点
+            if (zf >= 4 and price_zy > kline_hour[-1][2] and price_close >= price_vol >=
                     bollinger_band_upper([k[4] for k in kline_hour[:21]], 21)
                     and kline_hour[-2][5] >= statistics.mean([k[5] for k in kline_hour[:21]]) * 4):
-                alert.append((symbol, price_close, zf, price_vol * 1.06, price_close * 0.96))
+                alert.append((symbol, price_close, zf, price_zy, price_close * 0.96))
             success.add(symbol)
             break
         except Exception:
