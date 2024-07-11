72a63370fa3120e2226b3353437038dfbb0ba2b2
ylitchan <898475174@qq.com>
2024-07-11 13:37:57 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/RZQ.py
+++ tokenDemo/RZQ.py
@@ -74,7 +74,7 @@ def rzq_token(symbol, alert, success):
             price_zy = price_close + price_vol * 0.02
             zf = (price_vol / kline_hour[-3][4] - 1) * 100
             if (zf >= 1 and price_zy > kline_hour[-1][2] and price_close >= price_vol >= max(
-                    (kline_hour[-2][2] + kline_hour[-2][3]) / 2,
+                    (kline_hour[-2][2] + kline_hour[-2][3]) * 0.51,
                     bollinger_band_upper([k[4] for k in kline_hour[:20]]))
                     and kline_hour[-2][5] >= statistics.mean([k[5] for k in kline_hour[:20]]) * 3):
                 alert.append((symbol, price_close, zf, price_zy, kline_hour[-2][3]))
