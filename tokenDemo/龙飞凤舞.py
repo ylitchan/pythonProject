ae2deeb5441aeffd7ea8e7d461488f4b2cee5148
ylitchan <898475174@qq.com>
2024-07-26 16:40:08 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/龙飞凤舞.py
+++ tokenDemo/龙飞凤舞.py
@@ -13,7 +13,9 @@ from binance.spot import Spot
 from jsonpath_ng import parse
 from lxml import etree
 
+requests.packages.urllib3.disable_warnings()
 session = requests.Session()
+session.verify = False
 session.headers = {'Content-Type': 'application/json',
                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'}
 # 创建BlockingScheduler对象
