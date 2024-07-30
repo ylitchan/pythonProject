d91adf06a71349d6a7f0e7a8aefb7b0a473b2ced
ylitchan <898475174@qq.com>
2024-07-30 23:26:41 +0800

    y

--- tokenDemo/龙飞凤舞.py
+++ tokenDemo/龙飞凤舞.py
@@ -101,8 +101,8 @@ def rzq_token(symbol, alert, success):
             #                   client.klines(symbol=symbol, interval="1h", limit=21)]
             price_close = kline_hour[-1][4]
             price_vol = kline_hour[-2][4]
-            zf = (price_vol / kline_hour[-3][4] - 1)
-            price_zy = price_close + price_vol * min(0.05, zf * 0.5)
+            zf = price_vol / kline_hour[-3][4] - 1
+            price_zy = price_vol + price_vol * min(0.05, zf * 0.5)
             if ((zf >= 0.03 and price_zy > kline_hour[-1][2]
                  and price_vol >= max(bollinger_band_upper([k[4] for k in kline_hour[:20]]),
                                       (kline_hour[-2][2] + kline_hour[-2][3]) * 0.51)
