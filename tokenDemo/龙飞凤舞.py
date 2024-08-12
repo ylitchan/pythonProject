9211ab142748f17b058ba28abdece66b87ec5eba
ylitchan <898475174@qq.com>
2024-08-12 23:58:29 +0800

    y

--- tokenDemo/龙飞凤舞.py
+++ tokenDemo/龙飞凤舞.py
@@ -192,10 +192,10 @@ def main():
     rzq_market('BN', symbols_bn, rzq_token)
     rzq_market('OKX', symbols_okx, rzq_token)
     # 设置任务调度
-    scheduler.add_job(rzq_market, 'cron', hour='09', minute='25', second='00', timezone='Asia/Shanghai')
-    scheduler.add_job(rzq_market, 'cron', hour='*', minute='*/15', second='10', timezone='Asia/Shanghai',
+    scheduler.add_job(rzq_market, 'cron', hour='*', minute='*/15', second='00', timezone='Asia/Shanghai')
+    scheduler.add_job(rzq_market, 'cron', hour='*', minute='*/15', second='00', timezone='Asia/Shanghai',
                       args=['BN', symbols_bn, rzq_token])
-    scheduler.add_job(rzq_market, 'cron', hour='*', minute='*/15', second='10', timezone='Asia/Shanghai',
+    scheduler.add_job(rzq_market, 'cron', hour='*', minute='*/15', second='00', timezone='Asia/Shanghai',
                       args=['OKX', symbols_okx, rzq_token])
     # 启动调度器
     scheduler.start()
