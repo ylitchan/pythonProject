ff2c98693821d011d44a25a5b22d559aa11bdfbe
ylitchan <898475174@qq.com>
2024-09-18 09:11:12 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/突破战法.py
+++ tokenDemo/突破战法.py
@@ -41,9 +41,11 @@ def is_golden_cross(data, short_window=5, mid_window=10, long_window=20):
     mid_ma = statistics.mean(data[-mid_window:])
     long_ma = statistics.mean(data[-long_window:])
     # 判定是否满足多头排列条件
-    return short_ma >= mid_ma >= long_ma and data[-2] >= statistics.mean(data[-short_window - 1:-1]) and list(filter(
-        lambda x: statistics.mean(data[x[0] + 1 - short_window:x[0] + 1]) <= x[-1] <= data[x[0] - 1] if len(
-            data) - 1 > x[0] > len(data) - 1 - short_window else False, enumerate(data)))
+    return not list(filter(
+        lambda x: statistics.mean(data[x[0] + 1 - short_window:x[0] + 1]) > x[-1] if len(data) - 1 > x[0] > len(
+            data) - 1 - short_window else False, enumerate(data))) and list(filter(
+        lambda x: x[-1] <= data[x[0] - 1] if len(data) - 1 > x[0] > len(data) - 1 - short_window else False,
+        enumerate(data)))
 
 
 def get_kline(symbol, t: str):
