af5067adb0d7fbf92220b5e2d0107836c9506fdb
ylitchan <898475174@qq.com>
2024-11-01 13:35:12 +0800

    无上权柄
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/启明恒星.py
+++ tokenDemo/启明恒星.py
@@ -41,16 +41,16 @@ def is_golden_cross(data, short_window=5, mid_window=10, long_window=20):
     mid_ma = statistics.mean(data[-mid_window:])
     long_ma = statistics.mean(data[-long_window:])
     # 判定是否满足多头排列条件
-    return list(filter(
-        lambda x: statistics.mean(data[x[0] + 1 - short_window:x[0] + 1]) <= x[-1] <= data[x[0] - 1] if len(data) - 1 >
-                                                                                                        x[0] > len(
-            data) - 1 - short_window else False,
-        enumerate(data)))
-    # return not list(filter(
-    #     lambda x: statistics.mean(data[x[0] + 1 - short_window:x[0] + 1]) > x[-1] if len(data) - 1 > x[0] > len(
-    #         data) - 1 - short_window else False, enumerate(data))) and list(filter(
-    #     lambda x: x[-1] <= data[x[0] - 1] if len(data) - 1 > x[0] > len(data) - 1 - short_window else False,
+    # return list(filter(
+    #     lambda x: statistics.mean(data[x[0] + 1 - short_window:x[0] + 1]) <= x[-1] <= data[x[0] - 1] if len(data) - 1 >
+    #                                                                                                     x[0] > len(
+    #         data) - 1 - short_window else False,
     #     enumerate(data)))
+    return not list(filter(
+        lambda x: statistics.mean(data[x[0] + 1 - short_window:x[0] + 1]) > x[-1] if len(data) - 1 > x[0] > len(
+            data) - 1 - short_window else False, enumerate(data))) and list(filter(
+        lambda x: x[-1] <= data[x[0] - 1] if len(data) - 1 > x[0] > len(data) - 1 - short_window else False,
+        enumerate(data)))
 
 
 def get_kline(symbol, t: str):
@@ -85,7 +85,7 @@ def rzq_token(symbol, alert, success):
         price_zy5 = round(price_close + price_vol * 0.015, max_decimal)
         price_zy3 = round(price_close + price_vol * 0.03, max_decimal)
         success.add(symbol)
-        if price_close > max([k[2] for k in kline[-5:-1]]) and is_golden_cross(kline_close):
+        if zf >= 3 and price_close > max([k[2] for k in kline[-5:-1]]) and is_golden_cross(kline_close):
             alert.update({symbol: (price_close, zf, price_zy5, price_zy3)})
             return {symbol: (price_close, zf, price_zy5, price_zy3)}
     except:
