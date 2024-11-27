cd3ec1d259d13652771535cf9d5ed4260f328d6c
ylitchan <898475174@qq.com>
2024-11-27 11:34:25 +0800

    首阴反包
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/首阴反包.py
+++ tokenDemo/首阴反包.py
@@ -77,8 +77,9 @@ def rzq_token(symbol, alert, success):
         price_close = kline[-1][4]
         price_vol = kline[-2][1]
         zf = round((kline[-3][4] / kline[-5][1] - 1) * 100, 2)
-        price_zy = round(price_close + price_vol * zf / 300, max_decimal)
-        price_zs = round(price_close - price_vol * zf / 300, max_decimal)
+        zf_m = statistics.median([round((k[4] / k[1] - 1) * 100, 2) for k in kline[-5:-3]])
+        price_zy = round(price_close + price_vol * zf_m / 100, max_decimal)
+        price_zs = round(price_close - price_vol * zf_m / 100, max_decimal)
         success.add(symbol)
         if zf >= 0 and price_close > max([k[1] for k in kline[-2:-1]]) and is_golden_cross(kline_close):
             alert.update({symbol: (price_close, zf, price_zy, price_zs)})
