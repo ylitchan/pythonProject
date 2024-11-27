d31e99b490494c4ab523b4ad3a9587bbd3203912
ylitchan <898475174@qq.com>
2024-11-27 09:48:44 +0800

    反包切入
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/反包切入.py
+++ tokenDemo/反包切入.py
@@ -43,9 +43,10 @@ def is_golden_cross(data, short_window=5, mid_window=10, long_window=20):
     # 判定是否满足多头排列条件
     return data[-2] <= data[-3] and not list(filter(
         lambda x: statistics.mean(data[x[0] + 1 - short_window:x[0] + 1]) > x[-1] if len(data) - 1 > x[0] > len(
-            data) - 1 - short_window else False, enumerate(data))) and len(list(filter(
-        lambda x: x[-1] <= data[x[0] - 1] if len(data) - 1 > x[0] > len(data) - 1 - short_window else False,
-        enumerate(data)))) >= 1
+            data) - 1 - short_window else False, enumerate(data)))
+    #     and len(list(filter(
+    # lambda x: x[-1] <= data[x[0] - 1] if len(data) - 1 > x[0] > len(data) - 1 - short_window else False,
+    # enumerate(data)))) >= 1
 
 
 def get_kline(symbol, t: str):
@@ -80,7 +81,11 @@ def rzq_token(symbol, alert, success):
         price_zy = round(price_close + price_vol * 0.011, max_decimal)
         price_zs = round(price_close - price_vol * 0.03, max_decimal)
         success.add(symbol)
-        if zf >= 0 and price_close > max([k[2] for k in kline[-3:-1]]) and is_golden_cross(kline_close):
+        if price_f := [k[2] for k in kline[-5:-1] if k[4] <= k[1]]:
+            price_f = max(price_f)
+        else:
+            return
+        if zf >= 0 and price_close > price_f and is_golden_cross(kline_close):
             alert.update({symbol: (price_close, zf, price_zy, price_zs)})
             return {symbol: (price_close, zf, price_zy, price_zs)}
     except:
