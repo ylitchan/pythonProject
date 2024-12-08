a145fa23d7b8d140fe99cb20c539b7fe78b439b0
ylitchan <898475174@qq.com>
2024-12-08 21:31:23 +0800

    预期排序

--- tokenDemo/N字抄底.py
+++ tokenDemo/N字抄底.py
@@ -83,6 +83,8 @@ def rzq_token(symbol, alert, success):
         kline = get_kline(symbol, "1Dutc")
         kline_close = [k[4] for k in kline]
         success.add(symbol)
+        if kline[-2][4] > kline[-2][1]:
+            return
         index_s, index_e, kline_close_asc = find_continuous_subsequences(kline_close[::-1][1:], 1)
         if len(kline_close_asc) < 3 if kline[index_s][4] > kline[index_s][1] else len(kline_close_asc) < 4:
             return
