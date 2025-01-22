9a1a38f6c326ae740e6d75c9447c3aaffe26b3d7
ylitchan <898475174@qq.com>
2025-01-22 15:14:40 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/vote.py
+++ tokenDemo/vote.py
@@ -4,6 +4,8 @@ from flask_cors import CORS
 from lxml import etree
 
 app = Flask(__name__)
+# 设置请求体最大为 16MB
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024  # 16 MB
 CORS(app, supports_credentials=True)
 requests.packages.urllib3.disable_warnings()
 session = requests.Session()
@@ -65,4 +67,48 @@ def receive_html():
 
 
 if __name__ == '__main__':
+    html_data=''
+    alert = []
+    alert0 = []
+    print("Received HTML content:")
+    tree = etree.HTML(html_data)
+
+    # 这里写提取数据的代码
+
+    links = tree.xpath('//div[@class="css-1dveyv5"]')
+    for link in links:
+        content = link.xpath('.//div[@class="css-zxi8en"]//text()')
+        symbol = content[0]
+        voter = float(content[1].split('%')[0] if '%' in content[1] else '0')
+        bribe = float(content[2].split('%')[0] if '%' in content[2] else '0')
+        if bribe:
+            alert.append((symbol, voter, bribe, voter + bribe))
+        else:
+            alert0.append((symbol, voter, bribe, voter + bribe))
+    alert_sort = sorted(alert, key=lambda x: x[-1], reverse=True)[:3]
+    alert0_sort = sorted(alert0, key=lambda x: x[-1], reverse=True)[:3]
+    alert_final = []
+    for i, j in enumerate(alert_sort):
+        alert_final.append(
+            f'{i + 1}.{j[0]}\n'
+            f'Voter APY:{j[1]}\nBribe:{j[2]}\n总和:{j[3]}')
+    json_msg = {
+        "msgtype": "text",
+        "text": {'content': f'===bribe===\n' + '\n-------\n'.join(alert_final)}
+    }
+    session.post(
+        url='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=6f2ec864-c474-4c8f-b069-1e3c35eb7d73',
+        json=json_msg)
+    alert0_final = []
+    for i, j in enumerate(alert0_sort):
+        alert0_final.append(
+            f'{i + 1}.{j[0]}\n'
+            f'Voter APY:{j[1]}\nBribe:{j[2]}\n总和:{j[3]}')
+    json_msg = {
+        "msgtype": "text",
+        "text": {'content': f'===bribe0===\n' + '\n-------\n'.join(alert0_final)}
+    }
+    session.post(
+        url='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=6f2ec864-c474-4c8f-b069-1e3c35eb7d73',
+        json=json_msg)
     app.run(host='0.0.0.0', port=9000, debug=False)
---
+++ tokenDemo/投票预警.py
@@ -0,0 +1,70 @@
+import requests
+from DrissionPage import ChromiumPage, ChromiumOptions
+from apscheduler.schedulers.blocking import BlockingScheduler
+from lxml import etree
+
+requests.packages.urllib3.disable_warnings()
+session = requests.Session()
+session.verify = False
+session.headers = {'Content-Type': 'application/json',
+                   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'}
+# 创建浏览器配置对象，指定浏览器路径
+co = ChromiumOptions().set_browser_path(r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe')
+
+
+def job():
+    # 用该配置创建页面对象
+    page = ChromiumPage(addr_or_opts=co)
+    page.get('https://equilibria.fi/vote')
+    page.wait.ele_displayed('.css-1dveyv5', timeout=300)
+    html_data = page.html
+    page.quit()
+    alert = []
+    alert0 = []
+    print("Received HTML content:")
+    tree = etree.HTML(html_data)
+    # 这里写提取数据的代码
+
+    links = tree.xpath('//div[@class="css-1dveyv5"]')
+    for link in links:
+        content = link.xpath('.//div[@class="css-zxi8en"]//text()')
+        symbol = content[0]
+        voter = float(content[1].split('%')[0].replace('K', '000') if '%' in content[1] else '0')
+        bribe = float(content[2].split('%')[0].replace('K', '000') if '%' in content[2] else '0')
+        if bribe:
+            alert.append((symbol, voter, bribe, voter + bribe))
+        else:
+            alert0.append((symbol, voter, bribe, voter + bribe))
+    alert_sort = sorted(alert, key=lambda x: x[-1], reverse=True)[:3]
+    alert0_sort = sorted(alert0, key=lambda x: x[-1], reverse=True)[:3]
+    alert_final = []
+    for i, j in enumerate(alert_sort):
+        alert_final.append(
+            f'{i + 1}.{j[0]}\n'
+            f'Voter APY:{j[1]}\nBribe:{j[2]}\n总和:{j[3]}')
+    json_msg = {
+        "msgtype": "text",
+        "text": {'content': f'===bribe===\n' + '\n-------\n'.join(alert_final)}
+    }
+    session.post(
+        url='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=6f2ec864-c474-4c8f-b069-1e3c35eb7d73',
+        json=json_msg)
+    alert0_final = []
+    for i, j in enumerate(alert0_sort):
+        alert0_final.append(
+            f'{i + 1}.{j[0]}\n'
+            f'Voter APY:{j[1]}\nBribe:{j[2]}\n总和:{j[3]}')
+    json_msg = {
+        "msgtype": "text",
+        "text": {'content': f'===bribe0===\n' + '\n-------\n'.join(alert0_final)}
+    }
+    session.post(
+        url='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=6f2ec864-c474-4c8f-b069-1e3c35eb7d73',
+        json=json_msg)
+
+
+# job()
+scheduler = BlockingScheduler()
+scheduler.add_job(job, 'cron', day_of_week='wed', hour=17, minute='25-55/10')
+# 启动调度器
+scheduler.start()
