2db11f0735e3ad72e3167ff3dd9324f14e1cae03
ylitchan <898475174@qq.com>
2025-03-06 18:57:20 +0800

    y

--- tokenDemo/fundingRate.py
+++ tokenDemo/fundingRate.py
@@ -356,7 +356,7 @@ async def main():
             um_futures_client.renew_listen_key(listenKey=listenKey)
             time_now = datetime.now()
             print(time_now, f'renew listen key:{listenKey}')
-            if time_now.hour == 12 and time_now.minute <= 5:
+            if time_now.hour == 20 and time_now.minute <= 5:
                 with open('fundingRate.json', 'r+') as f:
                     excel_data = json.load(f)
                     if excel_data:
