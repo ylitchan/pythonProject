a16b30c1162065723b6eaa0e97181a745bc97568
ylitchan <898475174@qq.com>
2025-03-08 18:41:43 +0800

    y

--- tokenDemo/liquidationLybra.py
+++ tokenDemo/liquidationLybra.py
@@ -289,7 +289,7 @@ def send_msg(msg):
             "text": {'content': msg}
         }
         session.post(
-            url='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=ee6e64f7-4423-47d0-9f9c-583298d7ae02',
+            url='https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=6f2ec864-c474-4c8f-b069-1e3c35eb7d73',
             json=json_msg)
     except:
         return
