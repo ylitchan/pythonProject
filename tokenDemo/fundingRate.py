70dc63c33b18864a94c42df2b71c34e92632648a
ylitchan <898475174@qq.com>
2025-03-20 15:34:49 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/fundingRate.py
+++ tokenDemo/fundingRate.py
@@ -306,7 +306,8 @@ async def main():
             # 总账户余额（可用保证金 + 已占用保证金）
             balance_bn_total = float(account_data['totalMarginBalance'])
             balance_drift_total = drift_user.get_total_collateral(margin_category=None) / QUOTE_PRECISION
-            funding_rate_round = round(funding_rate / FUNDING_RATE_PRECISION / 24 * 1e2, PERCENTAGE_PRECISION_EXP)
+            funding_rate_round = round(funding_rate / event.data.oracle_price_twap / FUNDING_RATE_PRECISION * 1e2,
+                                       PERCENTAGE_PRECISION_EXP)
             symbol_funding = perp_market_indexes_symbol.get(event.data.market_index)
             excel_data_now = {'时间': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                               'symbol': symbol_funding,
