631f7f34f72cfab6eb6d05aaae435e8dcbbc1090
ylitchan <898475174@qq.com>
2025-04-24 11:48:00 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -40,7 +40,8 @@ def trade(symbol, price, stopPrice, symbols_info):
                     "side": "SELL",
                     "quantity": symbol_free_round,
                     "price": price,
-                    "stopPrice": stopPrice
+                    "stopPrice": max(stopPrice, float(
+                        Decimal(6 / symbol_free_round).quantize(Decimal(f'{stopPrice}'), rounding=ROUND_DOWN)))
                 }
                 spotBN.new_oco_order(**params)
                 return symbol
@@ -101,7 +102,7 @@ async def rzq_token(semaphore, symbol, alert, success, alert_m):
         if expectation <= 0:
             return
         if price_zy > kline[-1][2]:
-            price_zs = kline[index_e][3]
+            price_zs = max(kline[index_e][3], round(price_close * (1 - expectation / 100 * 2), max_decimal))
             data = {symbol: (price_close, round(zf_m, 2), expectation, price_zy, price_zs)}
             alert.update(data)
             alert_m.update(data)
