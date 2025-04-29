8f641aca8ecb79ad5a539ce52aaae60d46de74ca
ylitchan <898475174@qq.com>
2025-04-29 19:09:59 +0800

    y

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -96,7 +96,9 @@ async def rzq_token(semaphore, symbol, alert, success, alert_m):
         kline_close = [k[4] for k in kline]
         max_decimal = max(map(lambda ks: -Decimal(str(ks)).normalize().as_tuple().exponent, kline_close))
         kline_zf = list(map(lambda k: (k[4] / k[1] - 1) * 100, kline[index_s:-1]))
-        zf_m = calculate_ema_pandas(kline_zf)
+        size_z = len(list(filter(lambda k: k[4] > k[1], kline[index_s:-1])))
+        size_d = len(list(filter(lambda k: k[4] <= k[1], kline[index_s:-1])))
+        zf_m = calculate_ema_pandas(kline_zf) * size_z / (size_z + size_d)
         price_zy = round(kline[-2][4] + kline[-2][4] * zf_m / 100, max_decimal)
         expectation = round((price_zy / kline[-2][2] - 1) * 100, 2)
         if expectation <= 0:
