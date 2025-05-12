ff7f41ba50e526bfa6e004d587661d3536652370
ylitchan <898475174@qq.com>
2025-05-12 09:28:20 +0800

    y

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -161,7 +161,7 @@ async def rzq_token(semaphore, symbol, alert, success, alert_m):
         # 计算 K 线数据的涨跌幅列表
         kline_zf = list(map(lambda k: k[4] / k[1] - 1, kline[index_e:-1]))
         zf_z = calculate_ema_pandas([k if k > 0 else 0 for k in kline_zf]) * 0.9
-        zf_d = calculate_ema_pandas([k if k < 0 else 0 for k in kline_zf]) * 0.9
+        zf_d = calculate_ema_pandas([k if k < 0 else 0 for k in kline_zf])
         price_zy = round(kline[-2][4] + kline[-2][4] * zf_z, max_decimal)
         expectation = round((price_zy / kline[-1][2] - 1) * 100, 2)
         if expectation <= 0:
