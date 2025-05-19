1d208d97e66ecf6b93ac0ee3b81b3692b96d606e
ylitchan <898475174@qq.com>
2025-05-19 09:51:12 +0800

    y
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/挖lybra.py
+++ tokenDemo/挖lybra.py
@@ -43,7 +43,7 @@ def send_msg(msg):
 
 def get_close():
     current_time = int(datetime.now().timestamp())
-    response = requests.get("https://prices.curve.fi/v1/ohlc/ethereum/0x2673099769201c08E9A5e63b25FBaF25541A6557",
+    response = requests.get("https://prices.curve.finance/v1/ohlc/ethereum/0x2673099769201c08E9A5e63b25FBaF25541A6557",
                             params={
                                 'main_token': "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
                                 'reference_token': "0xdf3ac4F479375802A821f7b7b46Cd7EB5E4262cC",
@@ -52,16 +52,16 @@ def get_close():
                                 'start': current_time - 15 * 60,
                                 'end': current_time
                             })
-    response = requests.post(
-        'https://prod.ave-api.com/v2/tokens/price',
-        headers={
-            'X-API-KEY': 'ufzEUWscHCQLXkncSTe4eIAFyafdZNrnXYK9pfJAuNWSDjFmip50yxN5avvT4Rv4',
-        }, json={'token_ids': ['0x97de57ec338ab5d51557da3434828c5dbfada371-eth',
-                               '0xdf3ac4f479375802a821f7b7b46cd7eb5e4262cc-eth'], 'tvl_min': 0,
-                 'tx_24h_volume_min': 0}
-    )
+    # response = requests.post(
+    #     'https://prod.ave-api.com/v2/tokens/price',
+    #     headers={
+    #         'X-API-KEY': 'ufzEUWscHCQLXkncSTe4eIAFyafdZNrnXYK9pfJAuNWSDjFmip50yxN5avvT4Rv4',
+    #     }, json={'token_ids': ['0x97de57ec338ab5d51557da3434828c5dbfada371-eth',
+    #                            '0xdf3ac4f479375802a821f7b7b46cd7eb5e4262cc-eth'], 'tvl_min': 0,
+    #              'tx_24h_volume_min': 0}
+    # )
     price_close = response.json()['data'][-1]['close']
-    response2 = requests.get("https://prices.curve.fi/v1/ohlc/ethereum/0x880F2fB3704f1875361DE6ee59629c6c6497a5E3",
+    response2 = requests.get("https://prices.curve.finance/v1/ohlc/ethereum/0x880F2fB3704f1875361DE6ee59629c6c6497a5E3",
                              params={
                                  'main_token': "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
                                  'reference_token': "0x97de57eC338AB5d51557DA3434828C5DbFaDA371",
