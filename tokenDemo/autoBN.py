76bb9d2cc3134789459d6087c3cf3cdcf2494275
ylitchan <898475174@qq.com>
2025-05-30 19:06:18 +0800

    优化做多条件，暂时注释trade调用
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -308,10 +308,11 @@ async def rzq_token(semaphore, symbol, success, symbols_info):
         recent_vol_max = max(kline_vol[-10:])  # 近10天最大成交量
 
         # 做多条件：当日涨幅为近10天最大且成交量为近10天最高
-        if kline_zf[-1] >= recent_zf_max and kline_vol[-1] >= recent_vol_max:
+        if (kline_zf[-1] >= recent_zf_max and kline_vol[-1] >= recent_vol_max
+                and (kline[-1][2] - kline_close[-1]) / kline[-1][1] < kline_zf[-1] / 2):
             send_msg(f'==={symbol}做多===\n价格:{kline_close[-1]}\n涨幅:{kline_zf[-1]:.2%}')
             alert_all['POSITIONS'].append(symbol)
-            trade(symbol=symbol, symbols_info=symbols_info, slot=0.2)
+            # trade(symbol=symbol, symbols_info=symbols_info, slot=0.2)
 
         # 做空条件：
         # 1. 当日为阴线(涨跌幅为负)
