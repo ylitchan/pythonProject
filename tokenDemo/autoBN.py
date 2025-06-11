68c97b064b7292423c4c8b3c450c90383c9d98de
ylitchan <898475174@qq.com>
2025-06-11 14:21:06 +0800

    完善POSITIONS字典更新逻辑，新增空值占位符，修复条件判断后的状态同步问题
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -350,6 +350,8 @@ async def rzq_token(semaphore, symbol, success, symbols_info):
     :return: 若不符合条件则返回 None，否则返回符合条件的交易对信息字典
     """
     try:
+        if alert_all['POSITIONS'].get(symbol) == ():
+            return
         # 获取日K线数据
         kline = await get_kline(semaphore, symbol, "1Dutc")
         success.add(symbol)
@@ -362,7 +364,7 @@ async def rzq_token(semaphore, symbol, success, symbols_info):
         if close_info := alert_all['POSITIONS'].get(symbol):
             if kline_close[-1] >= close_info[0] or kline_close[1] <= close_info[-1]:
                 close_bn_position(symbol, close_info[2], close_info[3])
-                alert_all['POSITIONS'].pop(symbol)
+                alert_all['POSITIONS'][symbol] = ()
             return
         kline_vol = [k[5] for k in kline]  # 成交量列表
 
