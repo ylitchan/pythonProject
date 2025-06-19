54da3232b13f737bcf1f2f827ae86316318c755e
ylitchan <898475174@qq.com>
2025-06-19 11:36:29 +0800

    修正做多与做空逻辑中的止盈止损公式，调整计算基础以提升逻辑准确性与风控精度
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -304,8 +304,8 @@ async def rzq_token(semaphore, symbol, success, symbols_info):
                 kline_zf[-2] <= -recent_zf_max and  # 前一日涨幅最大
                 kline_vol[-2] >= 2 * recent_vol_max and  # 前一日成交量是前10天的2倍以上
                 (kline[-2][3] - kline_close[-2]) / kline[-2][1] >= kline_zf[-2] / 2):  # 下影线足够长
-            zy = kline_close[-1] - kline_zf[-3] * kline_zf[-2] * 0.2
-            zs = kline_close[-1] + kline_zf[-3] * kline_zf[-2] * 0.4
+            zy = kline_close[-1] - kline_close[-3] * kline_zf[-2] * 0.2
+            zs = kline_close[-1] + kline_close[-3] * kline_zf[-2] * 0.4
             send_msg(f'==={symbol}做多===\n价格:{kline_close[-1]}\n涨幅:{kline_zf[-1]:.2%}\n止盈:{zy}\n止损:{zs}')
             alert_all['POSITIONS'][symbol] = (zy, zs, 'SELL', 'LONG')
             open_bn_position(symbol, symbols_info, 'BUY', 'LONG')
@@ -318,8 +318,8 @@ async def rzq_token(semaphore, symbol, success, symbols_info):
               kline_zf[-2] >= recent_zf_max and  # 前一日涨幅最大
               kline_vol[-2] >= 2 * recent_vol_max and  # 前一日成交量是前10天的2倍以上
               (kline[-2][2] - kline_close[-2]) / kline[-2][1] >= kline_zf[-2] / 2):  # 上影线足够长
-            zy = kline_close[-1] - kline_zf[-3] * kline_zf[-2] * 0.2
-            zs = kline_close[-1] + kline_zf[-3] * kline_zf[-2] * 0.4
+            zy = kline_close[-1] - kline_close[-3] * kline_zf[-2] * 0.2
+            zs = kline_close[-1] + kline_close[-3] * kline_zf[-2] * 0.4
             send_msg(f'==={symbol}做空===\n价格:{kline_close[-1]}\n涨幅:{kline_zf[-1]:.2%}\n止盈:{zy}\n止损:{zs}')
             alert_all['POSITIONS'][symbol] = (zs, zy, 'BUY', 'SHORT')
             open_bn_position(symbol, symbols_info, 'SELL', 'SHORT')
