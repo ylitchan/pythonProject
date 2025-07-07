978fa43fa3a6fdcaa4070e332f5896778509d5f1
ylitchan <898475174@qq.com>
2025-07-07 16:53:52 +0800

    fix(tokenDemo): 修复消息发送中的错误描述问题
    - 将消息中的 {side} 替换为 {positionSide}，以正确显示交易方向
    - 此修改确保了交易通知消息中交易方向的准确性，提高了信息的可靠性
    
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -187,7 +187,7 @@ def open_bn_position(symbol, symbols_info, side, positionSide):
         )
 
         # 发送成功通知
-        msg = f'{symbol}成功{side}，杠杆:{actual_leverage}x，交易数量:{tx.get("origQty", 0)}，标记价格:{markPrice}'
+        msg = f'{symbol}成功{positionSide}，杠杆:{actual_leverage}x，交易数量:{tx.get("origQty", 0)}，标记价格:{markPrice}'
         send_msg(msg)
         return symbol
     except Exception as e:
