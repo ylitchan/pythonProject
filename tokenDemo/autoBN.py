2769d78acd1587f293c616e0a83bbd67e6063707
ylitchan <898475174@qq.com>
2025-07-08 11:13:34 +0800

    refactor(tokenDemo): 优化 autoBN.py 中的交易信号判断逻辑
    - 移除了未使用的成交量相关变量 recent_vol_max 和 recent_vol_max2
    - 简化了做多和做空条件判断，删除了与成交量相关的判断逻辑
    - 优化了代码结构，提高了代码可读性和维护性
    
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -299,8 +299,6 @@ async def rzq_token(semaphore, symbol, success, symbols_info, condition):
 
         # 计算关键指标
         recent_zf_max = max([abs(k) for k in kline_zf[-7:-1]])  # 近10天最大涨跌幅（绝对值）
-        recent_vol_max = max(kline_vol[-7:-2])  # 近10天最大成交量
-        recent_vol_max2 = max(kline_vol[-7:-3])  # 近10天最大成交量
         zy = kline_close[-1] - kline_close[-3] * kline_zf[-2] * 0.2
         zs = kline_close[-1] + kline_close[-3] * kline_zf[-2] * 0.2
         # 做多条件：
@@ -309,16 +307,14 @@ async def rzq_token(semaphore, symbol, success, symbols_info, condition):
         # 3. 前一日成交量为前10天的2倍以上
         # 4. 前一日下影线足够长（至少为涨幅的一半）
         if (kline_close[-1] >= kline[-2][1] and  # 当日为阳线
-                kline_zf[-2] <= -recent_zf_max  # and  # 前一日涨幅最大
-                # kline_vol[-2] >= 2 * recent_vol_max  # 前一日成交量是前10天的2倍以上
-        ):  # 下影线足够长
+                kline_zf[-2] <= -recent_zf_max  # 前一日涨幅最大
+        ):
             send_msg(f'==={symbol}做多===\n价格:{kline_close[-1]}\n涨幅:{kline_zf[-1]:.2%}\n止盈:{zy}\n止损:{zs}')
             alert_all['POSITIONS'][symbol] = (zy, zs, 'SELL', 'LONG')
             open_bn_position(symbol, symbols_info, 'BUY', 'LONG')
         elif (kline_close[-1] >= kline[-2][1] and 0 > max(kline_zf[-3:-1]) and
-              min(kline_zf[-3:-1]) <= -recent_zf_max  # and  # 前一日涨幅最大
-                # (kline_vol[-2] >= 2 * recent_vol_max or kline_vol[-3] >= 2 * recent_vol_max2)  # 前一日成交量是前10天的2倍以上
-        ):  # 下影线足够长
+              min(kline_zf[-3:-1]) <= -recent_zf_max  # 前一日涨幅最大
+        ):
             send_msg(f'==={symbol}做多===\n价格:{kline_close[-1]}\n涨幅:{kline_zf[-1]:.2%}\n止盈:{zy}\n止损:{zs}')
             alert_all['POSITIONS'][symbol] = (zy, zs, 'SELL', 'LONG')
             open_bn_position(symbol, symbols_info, 'BUY', 'LONG')
@@ -328,16 +324,14 @@ async def rzq_token(semaphore, symbol, success, symbols_info, condition):
         # 3. 前一日成交量为前10天的2倍以上
         # 4. 前一日上影线足够长（至少为涨幅的一半）
         elif (kline_close[-1] <= kline[-2][1] and 0 < min(kline_zf[-3:-1]) and
-              max(kline_zf[-3:-1]) >= recent_zf_max  # and  # 前一日涨幅最大
-                # (kline_vol[-2] >= 2 * recent_vol_max or kline_vol[-3] >= 2 * recent_vol_max2)  # 前一日成交量是前10天的2倍以上
-        ):  # 上影线足够长
+              max(kline_zf[-3:-1]) >= recent_zf_max  # 前一日涨幅最大
+        ):
             send_msg(f'==={symbol}做空===\n价格:{kline_close[-1]}\n涨幅:{kline_zf[-1]:.2%}\n止盈:{zy}\n止损:{zs}')
             alert_all['POSITIONS'][symbol] = (zs, zy, 'BUY', 'SHORT')
             open_bn_position(symbol, symbols_info, 'SELL', 'SHORT')
         elif (kline_close[-1] <= kline[-2][1] and  # 当日为阴线
-              kline_zf[-2] >= recent_zf_max  # and  # 前一日涨幅最大
-                # kline_vol[-2] >= 2 * recent_vol_max  # 前一日成交量是前10天的2倍以上
-        ):  # 上影线足够长
+              kline_zf[-2] >= recent_zf_max  # 前一日涨幅最大
+        ):
             send_msg(f'==={symbol}做空===\n价格:{kline_close[-1]}\n涨幅:{kline_zf[-1]:.2%}\n止盈:{zy}\n止损:{zs}')
             alert_all['POSITIONS'][symbol] = (zs, zy, 'BUY', 'SHORT')
             open_bn_position(symbol, symbols_info, 'SELL', 'SHORT')
