ee848fc1a01c6e7d73fe2bda6e39b608a66b4797
ylitchan <898475174@qq.com>
2025-07-11 14:58:10 +0800

    refactor(tokenDemo): 调整止盈止损策略参数
    - 将止盈止损的波动幅度参数从 0.8调整为 0.7- 此修改旨在优化风险管理，减少潜在损失
    
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -291,7 +291,7 @@ async def rzq_token(semaphore, symbol, success, symbols_info, condition):
                 kline_close[-1] >= recent_price_max  # 当日为阳线
         ):
             zy = kline_close[-1] + kline_close[-2] * kline_zf[-1] * 0.2
-            zs = kline_close[-1] - kline_close[-2] * min(0.8 / leverage, kline_zf[-1] * 0.8)
+            zs = kline_close[-1] - kline_close[-2] * min(0.7 / leverage, kline_zf[-1] * 0.7)
             send_msg(f'==={symbol}做多===\n价格:{kline_close[-1]}\n涨幅:{kline_zf[-1]:.2%}\n止盈:{zy}\n止损:{zs}')
             alert_all['POSITIONS'][symbol] = [zy, zs, 'SELL', 'LONG']
             open_bn_position(symbol, symbols_info, 'BUY', 'LONG')
@@ -299,7 +299,7 @@ async def rzq_token(semaphore, symbol, success, symbols_info, condition):
               kline_close[-1] <= recent_price_min  # 当日为阴线
         ):
             zy = kline_close[-1] + kline_close[-2] * kline_zf[-1] * 0.2
-            zs = kline_close[-1] - kline_close[-2] * max(-0.8 / leverage, kline_zf[-1] * 0.8)
+            zs = kline_close[-1] - kline_close[-2] * max(-0.7 / leverage, kline_zf[-1] * 0.7)
             send_msg(f'==={symbol}做空===\n价格:{kline_close[-1]}\n涨幅:{kline_zf[-1]:.2%}\n止盈:{zy}\n止损:{zs}')
             alert_all['POSITIONS'][symbol] = [zy, zs, 'BUY', 'SHORT']
             open_bn_position(symbol, symbols_info, 'SELL', 'SHORT')
