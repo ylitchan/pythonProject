eef570c3982117450fd31d5a523fb7dcd63819c2
ylitchan <898475174@qq.com>
2025-07-14 12:05:56 +0800

    refactor(tokenDemo): 调整自动做市定时任务执行频率
    - 将 autoBN.py 文件中的定时任务执行频率从每5 分钟调整为每 1 分钟
    - 此修改旨在提高任务执行的及时性和频率，以适应市场变化
    
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -640,7 +640,7 @@ async def main():
         rzq_market,  # 执行的函数
         'cron',  # 调度类型：按日历规则
         hour='*',  # 每天8点和20点
-        minute='01-59/5',  # 每1分钟
+        minute='01-59/1',  # 每1分钟
         second='*/30',  # 整点秒数
         timezone='Asia/Shanghai',  # 上海时区
         args=('BN',),  # 传递参数
