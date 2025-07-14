16dc87ef578ac0a024ee10430a336f0798274af3
ylitchan <898475174@qq.com>
2025-07-14 11:59:49 +0800

    refactor(tokenDemo): 调整自动做市策略执行频率- 将自动做市策略的执行频率从每分钟调整为每分钟内的指定秒数 - 修改 second 参数从 '*/15' 到 '*/150'，以减少策略执行次数 - 优化资源使用并减少对系统的频繁请求
    Signed-off-by: ylitchan <898475174@qq.com>

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -640,8 +640,8 @@ async def main():
         rzq_market,  # 执行的函数
         'cron',  # 调度类型：按日历规则
         hour='*',  # 每天8点和20点
-        minute='01-59/1',  # 每1分钟
-        second='*/15',  # 整点秒数
+        minute='01-59',  # 每1分钟
+        second='*/150',  # 整点秒数
         timezone='Asia/Shanghai',  # 上海时区
         args=('BN',),  # 传递参数
         misfire_grace_time=30,  # 错过执行的宽限时间（秒）
