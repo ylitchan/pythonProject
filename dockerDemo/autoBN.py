17ed1b10ac141c0265cde82c47766df01bfce0c5
ylitchan <898475174@qq.com>
2025-07-23 10:29:38 +0800

    refactor.dockerDemo: 更新 autoBN.py 中的 API 地址- 将硬编码的 IP地址替换为环境变量中的主机名 - 提高代码的可维护性和环境适应性
    Signed-off-by: ylitchan <898475174@qq.com>

--- dockerDemo/autoBN.py
+++ dockerDemo/autoBN.py
@@ -42,7 +42,7 @@ def send_msg(msg, wx=False):
                 {"AtWxIDList": ["string"], "ImageContent": "", "MsgType": 0, "TextContent": msg,
                  "ToUserName": "49124710049@chatroom"}]}
             response = session.post(
-                'http://192.168.144.199:1238/message/SendTextMessage?key=fe197940-30c1-4cea-a41a-17b461423f83',
+                'http://wechatpadpro:1238/message/SendTextMessage?key=fe197940-30c1-4cea-a41a-17b461423f83',
                 json=json_msg)
         else:
             # 构建企业微信消息格式
