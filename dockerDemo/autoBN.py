225501bb54dc82c87513592541cae578fe277d67
ylitchan <898475174@qq.com>
2025-07-28 10:33:14 +0800

    refactor(binance): 优化基差检测逻辑
    - 添加时间戳变量 time_now 以减少重复调用 time.time()
    - 修改基差超过2%的检测条件，增加时间间隔限制
    - 优化代码结构，提高可读性和性能

--- dockerDemo/autoBN.py
+++ dockerDemo/autoBN.py
@@ -350,13 +350,14 @@ async def if_basis():
             index_price = {ip['symbol']: float(ip['indexPrice']) for ip in index_price.json()}
             market_price = (await asyncio.to_thread(session.get,
                                                     url="https://fapi.binance.com/fapi/v2/ticker/price")).json()
+            time_now = time.time()
             # 一次性将所有数据转换为浮点数
             for p in market_price:
                 if (basis := index_price.get(p['symbol'], float(p['price'])) / float(p['price'])) > 1.015 and \
-                        time.time() - BASIS.get(p['symbol'], 0) > 180:
+                        time_now - BASIS.get(p['symbol'], 0) > 180:
                     BASIS[p['symbol']] = time.time()
                     send_msg(f'{p["symbol"]} 基差异常：{basis * 100 - 100:.2f}%', True)
-                elif basis >= 1.02:
+                elif basis >= 1.02 and time_now - BASIS.get(p['symbol'], 0) > 60:
                     BASIS[p['symbol']] = time.time()
                     send_msg(f'{p["symbol"]} 基差超过2%：{basis * 100 - 100:.2f}%', True)
         except:
