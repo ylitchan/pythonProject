6c1462276f8cc1359a3b57cb97aae5e61a7dc529
ylitchan <898475174@qq.com>
2025-10-01 22:28:42 +0800

    修正(autoBN): 合并开仓条件中的价格判断逻辑，简化代码以提高策略的准确性和可读性。

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -584,10 +584,8 @@ class AUTOBN:
             # 条件3：持仓量增加信号（increase_oi函数返回True）
             if kline_close[-3] < kline_close[-2] and max(kline[-3][5], kline[-4][5]) < kline[-2][5] and \
                     await self.increase_oi(semaphore, symbol, 'LONG'):
-                no_position = symbol not in self.alert_all['POSITIONS']
-                not_spike_long = kline[-1][1] <= kline_close[-1] <= kline[-1][2] < kline[-1][1]*1.05
                 allow_open_long = is_early_morning or (
-                    no_position and not_spike_long)
+                    symbol not in self.alert_all['POSITIONS'] and kline[-1][1] <= kline_close[-1] and kline[-1][2] < kline[-1][1]*1.05)
                 if allow_open_long:
                     # 设置止盈止损：止盈9%，止损9%
                     zy = kline_close[-1] * 1.05  # 止盈价
