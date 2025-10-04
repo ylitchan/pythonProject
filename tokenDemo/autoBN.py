10532d593dfac2f4a052b9b6cd77bd9e3346656a
ylitchan <898475174@qq.com>
2025-10-04 22:12:19 +0800

    修正(autoBN): 更新开仓条件中的持仓量判断逻辑，修正为相等关系以提高策略的准确性。

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -410,7 +410,7 @@ class AUTOBN:
                         # 如果持仓量和价值都增加，说明是正常增仓，继续等待
                         if (
                             kline_close[index-1] == max(kline_close) and
-                            sumOpenInterestValue[index] > max(
+                            sumOpenInterestValue[index] == max(
                                 sumOpenInterestValue) and
                             kline_close[-2] > max(kline_close[-5:-2]) and
                             sumOpenInterest[-1] < min(sumOpenInterest[-3:-1]) and
