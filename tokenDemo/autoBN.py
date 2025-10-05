708307e565170a2ce620e75d073a496329f5a582
ylitchan <898475174@qq.com>
2025-10-06 07:08:39 +0800

    修正(autoBN): 更新止盈价和止损价的计算逻辑，使用kline数据替代kline_close，以确保在不同市场条件下的准确性和策略优化。

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -628,8 +628,8 @@ class AUTOBN:
                 await self.increase_oi(semaphore, symbol, 'LONG')
             ):
                 # 设置止盈止损：止盈9%，止损9%
-                zy = kline_close[-1] * 1.05  # 止盈价
-                zs = kline_close[-1] * 0.95  # 止损价
+                zy = kline[-1][3] * 1.05  # 止盈价
+                zs = kline[-1][1] * 0.95  # 止损价
                 if close_info and close_info[3] == 'SHORT':
                     self.close_bn_position(
                         symbol, close_info[2], close_info[3], kline_close[-1], 1)
@@ -659,8 +659,8 @@ class AUTOBN:
                 await self.increase_oi(semaphore, symbol, 'SHORT', kline_close)
             ):
                 # 设置止盈止损：止盈9%，止损9%
-                zy = kline_close[-1] * 0.95  # 止盈价
-                zs = kline_close[-1] * 1.05  # 止损价
+                zy = kline[-1][2] * 0.95  # 止盈价
+                zs = kline[-1][1] * 1.05  # 止损价
                 if close_info and close_info[3] == 'LONG':
                     self.close_bn_position(
                         symbol, close_info[2], close_info[3], kline_close[-1], 1)
