5f315f5517103d04de09cc8f82e62df02b2c2be8
ylitchan <898475174@qq.com>
2025-10-14 09:13:28 +0800

    ``` feat(tokenDemo): 增加对K线成交量参数的支持
    在increase_oi方法中新增kline_volume参数，用于更精确地判断增仓信号。
    同时更新了相关调用逻辑，确保在做空条件判断中正确传入成交量数据。
    ```

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -339,7 +339,7 @@ class AUTOBN:
                     Decimal(str(close_amount)).quantize(self.symbols_info.get(symbol)['quantityPrecision'], rounding=ROUND_DOWN))
         return None
 
-    async def increase_oi(self, semaphore, symbol, positionSide, kline_close=None):
+    async def increase_oi(self, semaphore, symbol, positionSide, kline_close=None,kline_volume=None):
         """
         检查增仓信号，判断是否适合开仓
 
@@ -408,6 +408,7 @@ class AUTOBN:
                     return any(
                         (
                             kline_close[index-1] == max(kline_close) and
+                            kline_volume[index-1] == max(kline_volume) and
                             sumOpenInterestValue[index] == max(sumOpenInterestValue) and
                             kline_close[-2] > max(kline_close[-5:-2]) and
                             sumOpenInterest[-1] < min(sumOpenInterest[-3:-1]) and
@@ -652,8 +653,7 @@ class AUTOBN:
                         )
                     ) and
                     kline_close[-2] > kline_close[-1] and
-                    kline_volume[-2] > max(kline_volume[:-2]) and
-                    await self.increase_oi(semaphore, symbol, 'SHORT', kline_close)
+                    await self.increase_oi(semaphore, symbol, 'SHORT', kline_close,kline_volume)
                 ):
                     # 设置止盈止损：止盈9%，止损9%
                     zy = kline[-1][1] * 0.97  # 止盈价
