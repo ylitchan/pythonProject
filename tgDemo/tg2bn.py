fbd8a46db67481bdd5c27335ab94e980fe867504
ylitchan <898475174@qq.com>
2025-10-15 13:42:21 +0800

    ``` feat(tgDemo/tg2bn.py): 更新做多信号标识为"做多ob"
    将交易信号中的"做多"更新为"做多ob"以区分不同类型的交易操作。
    
    feat(tokenDemo/autoBN.py): 更新做多信号标识为"做多ob"
    
    将自动交易中的"做多"标识修改为"做多ob"，用于明确交易类型。
    ```

--- tgDemo/tg2bn.py
+++ tgDemo/tg2bn.py
@@ -226,7 +226,7 @@ class HandleMsg:
                     zy = kline[-1][1] * 1.03
                     zs = kline[-1][1] * 0.95
                     self.autobn.send_msg(
-                        f"==={symbol}做多===\n价格:{kline_close[-1]}\n止盈:{zy}\n止损:{zs}"
+                        f"==={symbol}做多ob===\n价格:{kline_close[-1]}\n止盈:{zy}\n止损:{zs}"
                     )
                     if self.autobn.open_bn_position(symbol, "BUY", "LONG", 0.03):
                         self.autobn.alert_all["POSITIONS"][symbol] = [
