a10fc7cb43935fd11a975afa6688c448eb108921
ylitchan <898475174@qq.com>
2025-10-16 12:22:25 +0800

    fix(autoBN): 调整做多止损比例从0.95到0.97
    修改了自动交易系统中做多仓位的止损计算逻辑，将原来的0.95调整为0.97，
    以优化风险控制策略，提高资金安全性。

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -728,7 +728,7 @@ class AUTOBN:
                     self.alert_all["OBSERVATIONS"].pop(symbol)
                 elif open_info[3] == "LONG" and kline_close[-1] > open_info[0]:
                     zy = kline_close[-1] * 1.03
-                    zs = kline_close[-1] * 0.95
+                    zs = kline_close[-1] * 0.97
                     self.send_msg(
                         f"==={symbol}**BZ**===\n价格:{kline_close[-1]}\n止盈:{zy}\n止损:{zs}"
                     )
