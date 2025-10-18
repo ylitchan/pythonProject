20bfff77143c4e01e804158c9d7b2ebd8cacdb1e
ylitchan <898475174@qq.com>
2025-10-18 09:48:01 +0800

    fix(tgDemo/tg2bn.py): 修正kline_volume和kline_close范围判断逻辑
    将kline_volume的范围判断从range(-2, -8, -2)修正为range(-2, -6, -2)，
    同时将kline_close的范围判断从range(-2, -9, -1)修正为range(-2, -7, -1)，
    以确保正确的数据范围比较。

--- tgDemo/tg2bn.py
+++ tgDemo/tg2bn.py
@@ -285,13 +285,13 @@ class HandleMsg:
                     * 9
                     < kline_volume[-2]
                     and all(
-                        kline_volume[x] > kline_volume[x - 2] for x in range(-2, -8, -2)
+                        kline_volume[x] > kline_volume[x - 2] for x in range(-2, -6, -2)
                     )
                     and all(
                         kline_close[x]
                         >= sum(kline_close[x - 6 : x + 1])
                         / len(kline_close[x - 6 : x + 1])
-                        for x in range(-2, -9, -1)
+                        for x in range(-2, -7, -1)
                     )
                 ):
                     zy = kline_close[-1] * (1 + kline_zf)
