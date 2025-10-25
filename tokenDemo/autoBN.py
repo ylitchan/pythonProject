ba7133001b652d8ad038c4b673c45d0bd36b69f9
ylitchan <898475174@qq.com>
2025-10-25 17:03:47 +0800

    fix(autoBN): 移除多余的成交量和价格判断条件
    移除了在判断买入信号时不必要的成交量和价格连续性检查逻辑，
    简化了条件判断，提高代码可读性和执行效率。

--- tokenDemo/autoBN.py
+++ tokenDemo/autoBN.py
@@ -554,13 +554,6 @@ class AUTOBN:
                         / len(kline_volume[: -int(len(kline_volume) * 2 / 3)])
                         * 9
                         < kline_volume[-2]
-                        and all(
-                            kline_volume[x] > kline_volume[x - 2]
-                            and kline_close[x]
-                            >= sum(kline_close[x - 9 : x + 1])
-                            / len(kline_close[x - 9 : x + 1])
-                            for x in range(-2, -6, -2)
-                        )
                     ):
                         return True
                     return False
